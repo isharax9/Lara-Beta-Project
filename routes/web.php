6724e4f78421d1b3aba30ba33875fb62fdacc87f
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-12-05 16:16:48 +0530

    Learn about Starter kits, running raw SQL quarries
    📌Added Breeze & Blade library and added registration form, and login form(connected to local DB)
    📌Running raw SQL quarries.
    📌Learned MVC architecture

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::view('/', 'welcome');  
-Route::get('/', function(){
+Route::get('/', function () {
     return view('welcome');
+    //fetch all users 
+    // $users = DB::select("SELECT * FROM users");
+    // dd($users);
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
